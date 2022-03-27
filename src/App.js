06f7af330374059117e55777e1707253462b4157
Thilip Dhanavel <87736175+thilipd@users.noreply.github.com>
2022-03-27 11:40:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CrudComponent from './CrudComponenet';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CrudComponent />
+    </div>
+  );
+}
+
+export default App;
